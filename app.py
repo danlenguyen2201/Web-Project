0f52659b2e2e5911f6caf5c9247cc2afc1e3460d
Admin-PC0724ahc\Admin <danlenguyen2201@gmail.com>
2017-11-02 19:43:07 +0700

    f

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import *
 import mlab
 from mongoengine import *
 from faker import Faker
@@ -13,14 +13,21 @@ class Girl(Document):
     description = StringField()
     rating = FloatField()
 
-# f = Faker()
+
+
+
+#Mongo
+#1 Find record based on id
+#mongoengine lazy loading
+# girl=Girl.objects().with_id("59e32cc071c3060abca5dad6")
 #
-# for _ in range (20):
-#     g = Girl(name = f.name(),
-#             image = "http://via.placeholder.com/500x300",
-#             description = f.text(),
-#             rating = 4.1)
-#     g.save()
+# #2 Delete
+# if girl is None:
+#     print("Not Found")
+# else:
+#     girl.delete()
+
+
 
 @app.route("/")
 def index():
@@ -42,6 +49,56 @@ def list_demo():
 @app.route("/css-demo")
 def css_demo():
     return render_template("css_demo.html")
+
+@app.route("/admin")
+def admin_page():
+    girl_list = Girl.objects()
+    return render_template("admin.html", girls=girl_list)
+
+
+@app.route("/sign up")
+def signup_page():
+    return render_template("sign_up.html")
+
+@app.route("/addon",methods=["GET","POST"])
+def add():
+    if request.method == "GET":
+        return render_template("add_girl.html")
+    elif request.method == "POST":
+        form = request.form
+        name = form["name"]
+        image = form["image"]
+        description = form["description"]
+
+        girl=Girl(name=name,description=description,image=image,rating=1.2)
+        girl.save()
+
+        return "Added"
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/delete-girl/<girl_id>")
+def delete_girl(girl_id):
+    girl = Girl.objects().with_id(girl_id)
+    if girl is None:
+        return "Cannot found"
+    else:
+        return "Deleted" + girl_id
+
+@app.route("/edit-girl/<girl_id>",methods = ["GET","POST"])
+def edit_girl(girl_id):
+    girl = Girl.objects().with_id(girl_id)
+    if request.method == "GET":
+        return render_template("edit.html",g=girl)
+    elif request.method == "POST":
+        formm = request.form
+        name = form["name"]
+        image = form["image"]
+        description = form["description"]
+        girl.update(set_name=name, set_iamge=image, set_description=description)
+        return redirect("/admin")
 
 if __name__ == "__main__":
     app.run(debug=True)
